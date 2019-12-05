85541ee1d33d8d73d2dde4ddfac7ded014436371
pierre golfier <pierre@codeur.com>
2019-12-05 19:29:19 +0100

    duplicate builtin example and rename examples

---
+++ examples/builtin-simple/index.js
@@ -0,0 +1,20 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+import VueHandyGa from '@/vue-handy-ga';
+
+import App from './App.vue';
+
+Vue.use(Vuex);
+Vue.use(VueHandyGa);
+
+const options = {
+  //Add your own gaID
+  gaID: 'UA-119401025-6'
+};
+
+new Vue({
+  el: '#app',
+  store: new Vuex.Store(),
+  vueHandyGaSettings: new VueHandyGa(options),
+  render: createElement => createElement(App)
+});
